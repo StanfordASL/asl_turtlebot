3821e41a63f475065389f43226457a2605ea844c
Toki Migimatsu <toki.migimatsu@gmail.com>
2019-10-17 09:58:42 -0700

    Update for hw3

--- scripts/pose_controller.py
+++ scripts/pose_controller.py
@@ -6,146 +6,138 @@ from geometry_msgs.msg import Twist, PoseArray, Pose2D
 from std_msgs.msg import Float32MultiArray, String
 import tf
 import numpy as np
-from numpy import linalg
-from utils import wrapToPi
 
-# control gains
-K1 = 0.4
-K2 = 0.8
-K3 = 0.8
+from HW1.P2_pose_stabilization import PoseController
 
-# tells the robot to stay still
-# if it doesn't get messages within that time period
-TIMEOUT = 1.0
+class PoseControllerParams:
 
-# maximum velocity
-V_MAX = 0.2
+    def __init__(self, verbose=False):
+        # If sim is True (i.e. using gazebo), we want to subscribe to
+        # /gazebo/model_states. Otherwise, we will use a TF lookup.
+        self.use_gazebo = rospy.get_param("sim")
 
-# maximim angular velocity
-W_MAX = 1
+        # If using gmapping, we will have a map frame. Otherwise, it will be odom frame.
+        self.mapping = rospy.get_param("map")
 
-# if sim is True/using gazebo, therefore want to subscribe to /gazebo/model_states\
-# otherwise, they will use a TF lookup (hw2+)
-use_gazebo = rospy.get_param("sim")
+        # Control gains
+        self.k1 = rospy.get_param("~k1", 0.4)
+        self.k2 = rospy.get_param("~k2", 0.8)
+        self.k3 = rospy.get_param("~k3", 0.8)
 
-# if using gmapping, you will have a map frame. otherwise it will be odom frame
-mapping = rospy.get_param("map")
+        # Maximum velocity
+        self.v_max = rospy.get_param("~v_max", 0.2)
 
+        # Maximum angular velocity
+        self.om_max = rospy.get_param("~om_max", 1.)
 
-print "pose_controller settings:\n"
-print "use_gazebo = %s\n" % use_gazebo
-print "mapping = %s\n" % mapping
+        # Tells the robot to stay still if it doesn't get messages for TIMEOUT seconds.
+        self.timeout = rospy.get_param("~timeout", 1.)
 
-class PoseController:
+        if verbose:
+            print("PoseControllerParams:")
+            print("    use_gazebo = {}".format(self.use_gazebo))
+            print("    mapping = {}".format(self.mapping))
+            print("    k1, k2, k3 = {}, {}, {}".format(self.k1, self.k2, self.k3))
+            print("    v_max, om_max = {}, {}".format(self.v_max, self.om_max))
+            print("    timeout = {}".format(self.timeout))
+
+class PoseControllerNode:
 
     def __init__(self):
+        # Initialize ROS node
         rospy.init_node('turtlebot_pose_controller', anonymous=True)
+        self.params = PoseControllerParams(verbose=True)
+
+        # Current state
+        self.x = 0.
+        self.y = 0.
+        self.theta = 0.
+
+        # Controller from HW1 P2
+        self.controller = PoseController(self.params.k1, self.params.k2, self.params.k3,
+                                         self.params.v_max, self.params.om_max)
+
+        self.controller.load_goal(self.x, self.y, self.theta)
+
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
-        # current state
-        self.x = 0.0
-        self.y = 0.0
-        self.theta = 0.0
-
-        # goal state
-        self.x_g = 0.0
-        self.y_g = 0.0
-        self.theta_g = 0.0
-
-        
-
-        # time last pose command was received
+        # Time last pose command was received
         self.cmd_pose_time = rospy.get_rostime()
-        # if using gazebo, then subscribe to model states
-        if use_gazebo:
+
+        # If using gazebo, we have access to perfect state
+        if self.params.use_gazebo:
             rospy.Subscriber('/gazebo/model_states', ModelStates, self.gazebo_callback)
- 
         self.trans_listener = tf.TransformListener()
 
+        ########## Code starts here ##########
+        # TODO: Create a subscriber to the '/cmd_pose' topic that receives
+        #       Pose2D messages and calls cmd_pose_callback.
 
-        ######### YOUR CODE HERE ############
-        # create a subscriber that receives Pose2D messages and
-        # calls cmd_pose_callback. It should subscribe to '/cmd_pose'
-
-
-
-
-        ######### END OF YOUR CODE ##########
+        ########## Code ends here ##########
 
 
+    def gazebo_callback(self, msg):
+        if "turtlebot3_burger" not in msg.name:
+            return
 
-    def gazebo_callback(self, data):
-        if "turtlebot3_burger" in data.name:
-            pose = data.pose[data.name.index("turtlebot3_burger")]
-            twist = data.twist[data.name.index("turtlebot3_burger")]
-            self.x = pose.position.x
-            self.y = pose.position.y
-            quaternion = (
-                pose.orientation.x,
-                pose.orientation.y,
-                pose.orientation.z,
-                pose.orientation.w)
-            euler = tf.transformations.euler_from_quaternion(quaternion)
-            self.theta = euler[2]
-
-    def cmd_pose_callback(self, data):
-        ######### YOUR CODE HERE ############
-        # fill out cmd_pose_callback
+        pose = msg.pose[msg.name.index("turtlebot3_burger")]
+        self.x = pose.position.x
+        self.y = pose.position.y
+        quaternion = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+        self.theta = euler[2]
 
 
+    def cmd_pose_callback(self, msg):
+        ########## Code starts here ##########
+        # TODO: Update the goal pose in the pose controller.
 
+        ########## Code ends here ##########
 
-        ######### END OF YOUR CODE ##########
+        # Record time of pose update
         self.cmd_pose_time = rospy.get_rostime()
 
 
-    def get_ctrl_output(self):
-        """ runs a simple feedback pose controller """
-        if (rospy.get_rostime().to_sec()-self.cmd_pose_time.to_sec()) < TIMEOUT:
-            # if you are not using gazebo, your need to use a TF look-up to find robot's states
-            # relevant for hw 3+
-            if not use_gazebo:
-                try:
-                    origin_frame = "/map" if mapping else "/odom"
-                    (translation,rotation) = self.trans_listener.lookupTransform(origin_frame, '/base_footprint', rospy.Time(0))
-                    self.x = translation[0]
-                    self.y = translation[1]
-                    euler = tf.transformations.euler_from_quaternion(rotation)
-                    self.theta = euler[2]
-                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                    pass
-
-            ######### YOUR CODE HERE ############
-            # robot's state is self.x, self.y, self.theta
-            # robot's desired state is self.x_g, self.y_g, self.theta_g
-            # fill out cmd_x_dot = ... cmd_theta_dot = ...
-
-
-
-
-
-            ######### END OF YOUR CODE ##########
-
-        else:
+    def compute_control(self):
+        if rospy.get_rostime().to_sec() - self.cmd_pose_time.to_sec() > self.params.timeout:
             # haven't received a command in a while so stop
             rospy.loginfo("Pose controller TIMEOUT: commanding zero controls")
-            cmd_x_dot = 0
-            cmd_theta_dot = 0
+            cmd = Twist()
+            cmd.linear.x = 0.
+            cmd.angular.z = 0.
+            return cmd
 
+        # if you are not using gazebo, your need to use a TF look-up to find robot's states
+        # relevant for hw 3+
+        if not self.params.use_gazebo:
+            try:
+                origin_frame = "/map" if self.params.mapping else "/odom"
+                translation, rotation = self.trans_listener.lookupTransform(origin_frame, '/base_footprint', rospy.Time(0))
+                self.x, self.y = translation[0], translation[1]
+                self.theta = tf.transformations.euler_from_quaternion(rotation)[2]
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                pass
+
+        ######### YOUR CODE HERE ############
+        # TODO: Use your pose controller to compute controls (V, om) given the
+        #       robot's current state.
+
+        ######### END OF YOUR CODE ##########
 
         cmd = Twist()
-        cmd.linear.x = cmd_x_dot
-        cmd.angular.z = cmd_theta_dot
+        cmd.linear.x = V
+        cmd.angular.z = om
         return cmd
+
 
     def run(self):
         rate = rospy.Rate(10) # 10 Hz
         while not rospy.is_shutdown():
-            ctrl_output = self.get_ctrl_output()
+            ctrl_output = self.compute_control()
             self.pub.publish(ctrl_output)
             rate.sleep()
 
-if __name__ == '__main__':
 
-    pctrl = PoseController()
+if __name__ == '__main__':
+    pctrl = PoseControllerNode()
     pctrl.run()
--- scripts/supervisor.py
+++ scripts/supervisor.py
@@ -1,40 +1,16 @@
 #!/usr/bin/env python
 
-import rospy
-from gazebo_msgs.msg import ModelStates
-from std_msgs.msg import Float32MultiArray, String
-from geometry_msgs.msg import Twist, PoseArray, Pose2D, PoseStamped
-from asl_turtlebot.msg import DetectedObject
-import tf
-import math
 from enum import Enum
 
-# if sim is True/using gazebo, therefore want to subscribe to /gazebo/model_states\
-# otherwise, they will use a TF lookup (hw2+)
-use_gazebo = rospy.get_param("sim")
+import rospy
+from asl_turtlebot.msg import DetectedObject
+from gazebo_msgs.msg import ModelStates
+from geometry_msgs.msg import Twist, PoseArray, Pose2D, PoseStamped
+from std_msgs.msg import Float32MultiArray, String
+import tf
 
-# how is nav_cmd being decided -- human manually setting it, or rviz
-rviz = rospy.get_param("rviz")
-
-# if using gmapping, you will have a map frame. otherwise it will be odom frame
-mapping = rospy.get_param("map")
-
-
-# threshold at which we consider the robot at a location
-POS_EPS = .1
-THETA_EPS = .3
-
-# time to stop at a stop sign
-STOP_TIME = 3
-
-# minimum distance from a stop sign to obey it
-STOP_MIN_DIST = .5
-
-# time taken to cross an intersection
-CROSSING_TIME = 3
-
-# state machine modes, not all implemented
 class Mode(Enum):
+    """State machine modes. Feel free to change."""
     IDLE = 1
     POSE = 2
     STOP = 3
@@ -43,70 +19,122 @@ class Mode(Enum):
     MANUAL = 6
 
 
-print "supervisor settings:\n"
-print "use_gazebo = %s\n" % use_gazebo
-print "rviz = %s\n" % rviz
-print "mapping = %s\n" % mapping
+class SupervisorParams:
+
+    def __init__(self, verbose=False):
+        # If sim is True (i.e. using gazebo), we want to subscribe to
+        # /gazebo/model_states. Otherwise, we will use a TF lookup.
+        self.use_gazebo = rospy.get_param("sim")
+
+        # How is nav_cmd being decided -- human manually setting it, or rviz
+        self.rviz = rospy.get_param("rviz")
+
+        # If using gmapping, we will have a map frame. Otherwise, it will be odom frame.
+        self.mapping = rospy.get_param("map")
+
+        # Threshold at which we consider the robot at a location
+        self.pos_eps = rospy.get_param("~pos_eps", 0.1)
+        self.theta_eps = rospy.get_param("~theta_eps", 0.3)
+
+        # Time to stop at a stop sign
+        self.stop_time = rospy.get_param("~stop_time", 3.)
+
+        # Minimum distance from a stop sign to obey it
+        self.stop_min_dist = rospy.get_param("~stop_min_dist", 0.5)
+
+        # Time taken to cross an intersection
+        self.crossing_time = rospy.get_param("~crossing_time", 3.)
+
+        if verbose:
+            print("SupervisorParams:")
+            print("    use_gazebo = {}".format(self.use_gazebo))
+            print("    rviz = {}".format(self.rviz))
+            print("    mapping = {}".format(self.mapping))
+            print("    pos_eps, theta_eps = {}, {}".format(self.pos_eps, self.theta_eps))
+            print("    stop_time, stop_min_dist, crossing_time = {}, {}, {}".format(self.stop_time, self.stop_min_dist, self.crossing_time))
+
 
 class Supervisor:
 
     def __init__(self):
+        # Initialize ROS node
         rospy.init_node('turtlebot_supervisor', anonymous=True)
-        # initialize variables
+        self.params = SupervisorParams(verbose=True)
+
+        # Current state
         self.x = 0
         self.y = 0
         self.theta = 0
+
+        # Goal state
+        self.x_g = 0
+        self.y_g = 0
+        self.theta_g = 0
+
+        # Current mode
         self.mode = Mode.IDLE
-        self.last_mode_printed = None
-        self.trans_listener = tf.TransformListener()
-        # command pose for controller
+        self.prev_mode = None  # For printing purposes
+
+        ########## PUBLISHERS ##########
+
+        # Command pose for controller
         self.pose_goal_publisher = rospy.Publisher('/cmd_pose', Pose2D, queue_size=10)
-        # command vel (used for idling)
+
+        # Command vel (used for idling)
         self.cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
-        # subscribers
-        # stop sign detector
+        ########## SUBSCRIBERS ##########
+
+        # Stop sign detector
         rospy.Subscriber('/detector/stop_sign', DetectedObject, self.stop_sign_detected_callback)
-        # high-level navigation pose
+
+        # High-level navigation pose
         rospy.Subscriber('/nav_pose', Pose2D, self.nav_pose_callback)
-        # if using gazebo, we have access to perfect state
-        if use_gazebo:
+
+        # If using gazebo, we have access to perfect state
+        if self.params.use_gazebo:
             rospy.Subscriber('/gazebo/model_states', ModelStates, self.gazebo_callback)
-        # if using rviz, we can subscribe to nav goal click
-        if rviz:
+        self.trans_listener = tf.TransformListener()
+
+        # If using rviz, we can subscribe to nav goal click
+        if self.params.rviz:
             rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.rviz_goal_callback)
+        else:
+            self.x_g, self.y_g, self.theta_g = 1.5, -4., 0.
+            self.mode = Mode.NAV
         
+
+    ########## SUBSCRIBER CALLBACKS ##########
+
     def gazebo_callback(self, msg):
+        if "turtlebot3_burger" not in msg.name:
+            return
+
         pose = msg.pose[msg.name.index("turtlebot3_burger")]
-        twist = msg.twist[msg.name.index("turtlebot3_burger")]
         self.x = pose.position.x
         self.y = pose.position.y
-        quaternion = (
-                    pose.orientation.x,
-                    pose.orientation.y,
-                    pose.orientation.z,
-                    pose.orientation.w)
+        quaternion = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
         euler = tf.transformations.euler_from_quaternion(quaternion)
         self.theta = euler[2]
 
     def rviz_goal_callback(self, msg):
         """ callback for a pose goal sent through rviz """
-        origin_frame = "/map" if mapping else "/odom"
-        print("rviz command received!")
+        origin_frame = "/map" if self.params.mapping else "/odom"
+        print("Rviz command received!")
+
         try:
-            
             nav_pose_origin = self.trans_listener.transformPose(origin_frame, msg)
             self.x_g = nav_pose_origin.pose.position.x
             self.y_g = nav_pose_origin.pose.position.y
-            quaternion = (
-                    nav_pose_origin.pose.orientation.x,
-                    nav_pose_origin.pose.orientation.y,
-                    nav_pose_origin.pose.orientation.z,
-                    nav_pose_origin.pose.orientation.w)
+            quaternion = (nav_pose_origin.pose.orientation.x,
+                          nav_pose_origin.pose.orientation.y,
+                          nav_pose_origin.pose.orientation.z,
+                          nav_pose_origin.pose.orientation.w)
             euler = tf.transformations.euler_from_quaternion(quaternion)
             self.theta_g = euler[2]
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             pass
+
         self.mode = Mode.NAV
 
     def nav_pose_callback(self, msg):
@@ -123,15 +151,15 @@ class Supervisor:
         dist = msg.distance
 
         # if close enough and in nav mode, stop
-        if dist > 0 and dist < STOP_MIN_DIST and self.mode == Mode.NAV:
+        if dist > 0 and dist < self.params.stop_min_dist and self.mode == Mode.NAV:
             self.init_stop_sign()
 
 
-    ############ your code starts here ############
-    # feel free to change the code here 
-    # you may or may not find these functions useful
-    # there is no "one answer"
+    ########## STATE MACHINE ACTIONS ##########
 
+    ########## Code starts here ##########
+    # Feel free to change the code here. You may or may not find these functions
+    # useful. There is no single "correct implementation".
 
     def go_to_pose(self):
         """ sends the current desired pose to the pose controller """
@@ -159,10 +187,12 @@ class Supervisor:
         vel_g_msg = Twist()
         self.cmd_vel_publisher.publish(vel_g_msg)
 
-    def close_to(self,x,y,theta):
+    def close_to(self, x, y, theta):
         """ checks if the robot is at a pose within some threshold """
 
-        return (abs(x-self.x)<POS_EPS and abs(y-self.y)<POS_EPS and abs(theta-self.theta)<THETA_EPS)
+        return abs(x - self.x) < self.params.pos_eps and \
+               abs(y - self.y) < self.params.pos_eps and \
+               abs(theta - self.theta) < self.params.theta_eps
 
     def init_stop_sign(self):
         """ initiates a stop sign maneuver """
@@ -173,7 +203,8 @@ class Supervisor:
     def has_stopped(self):
         """ checks if stop sign maneuver is over """
 
-        return (self.mode == Mode.STOP and (rospy.get_rostime()-self.stop_sign_start)>rospy.Duration.from_sec(STOP_TIME))
+        return self.mode == Mode.STOP and \
+               rospy.get_rostime() - self.stop_sign_start > rospy.Duration.from_sec(self.params.stop_time)
 
     def init_crossing(self):
         """ initiates an intersection crossing maneuver """
@@ -184,74 +215,73 @@ class Supervisor:
     def has_crossed(self):
         """ checks if crossing maneuver is over """
 
-        return (self.mode == Mode.CROSS and (rospy.get_rostime()-self.cross_start)>rospy.Duration.from_sec(CROSSING_TIME))
+        return self.mode == Mode.CROSS and \
+               rospy.get_rostime() - self.cross_start > rospy.Duration.from_sec(self.params.crossing_time)
+
+    ########## Code ends here ##########
+
+
+    ########## STATE MACHINE LOOP ##########
 
     def loop(self):
         """ the main loop of the robot. At each iteration, depending on its
         mode (i.e. the finite state machine's state), if takes appropriate
         actions. This function shouldn't return anything """
 
-
-
-        #################################################################################
-        # Do not change this for hw2 -- this won't affect your FSM since you are using gazebo
-        if not use_gazebo:
+        if not self.params.use_gazebo:
             try:
                 origin_frame = "/map" if mapping else "/odom"
-                (translation,rotation) = self.trans_listener.lookupTransform(origin_frame, '/base_footprint', rospy.Time(0))
-                self.x = translation[0]
-                self.y = translation[1]
-                euler = tf.transformations.euler_from_quaternion(rotation)
-                self.theta = euler[2]
+                translation, rotation = self.trans_listener.lookupTransform(origin_frame, '/base_footprint', rospy.Time(0))
+                self.x, self.y = translation[0], translation[1]
+                self.theta = tf.transformations.euler_from_quaternion(rotation)[2]
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 pass
-        #################################################################################
-
-    # YOUR STATE MACHINE
-    # Currently it will just go to the pose without stopping at the stop sign
 
         # logs the current mode
-        if not(self.last_mode_printed == self.mode):
-            rospy.loginfo("Current Mode: %s", self.mode)
-            self.last_mode_printed = self.mode
+        if self.prev_mode != self.mode:
+            rospy.loginfo("Current mode: %s", self.mode)
+            self.prev_mode = self.mode
 
-        # checks wich mode it is in and acts accordingly
+        ########## Code starts here ##########
+        # TODO: Currently the state machine will just go to the pose without stopping
+        #       at the stop sign.
+
         if self.mode == Mode.IDLE:
-            # send zero velocity
+            # Send zero velocity
             self.stay_idle()
 
         elif self.mode == Mode.POSE:
-            # moving towards a desired pose
-            if self.close_to(self.x_g,self.y_g,self.theta_g):
+            # Moving towards a desired pose
+            if self.close_to(self.x_g, self.y_g, self.theta_g):
                 self.mode = Mode.IDLE
             else:
                 self.go_to_pose()
 
         elif self.mode == Mode.STOP:
-            # at a stop sign
+            # At a stop sign
             self.nav_to_pose()
 
         elif self.mode == Mode.CROSS:
-            # crossing an intersection
+            # Crossing an intersection
             self.nav_to_pose()
 
         elif self.mode == Mode.NAV:
-            if self.close_to(self.x_g,self.y_g,self.theta_g):
+            if self.close_to(self.x_g, self.y_g, self.theta_g):
                 self.mode = Mode.IDLE
             else:
                 self.nav_to_pose()
 
         else:
-            raise Exception('This mode is not supported: %s'
-                % str(self.mode))
+            raise Exception("This mode is not supported: {}".format(str(self.mode)))
 
-    ############ your code ends here ############
+        ############ Code ends here ############
 
     def run(self):
         rate = rospy.Rate(10) # 10 Hz
         while not rospy.is_shutdown():
             self.loop()
             rate.sleep()
+
 
 if __name__ == '__main__':
     sup = Supervisor()
