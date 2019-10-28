de394aee0b0d2020263f67ba2defb66dd4a64d16
Apoorva Sharma <apoorvaxsharma@gmail.com>
2019-10-28 09:37:50 -0700

    starter code for section 5

---
+++ scripts/controllers/utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pickle
+import os
+
+
+def wrapToPi(a):
+    if isinstance(a, list):    # backwards compatibility for lists (distinct from np.array)
+        return [(x + np.pi) % (2*np.pi) - np.pi for x in a]
+    return (a + np.pi) % (2*np.pi) - np.pi
