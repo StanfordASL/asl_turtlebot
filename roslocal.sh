e0f3f41b761b7cd437da1c3dce3a3c59f40462a1
Stanford U AA274A Fall 2019 Group 14 <group14@genbu.stanford.edu>
2019-11-03 15:40:50 -0800

    Generalize roslocal.sh

--- roslocal.sh
+++ roslocal.sh
@@ -1,3 +1,8 @@
-export ROS_MASTER_URI=http://localhost:11311
+HOSTNAME=$(hostname)
+if [[ ${HOSTNAME} == "genbu.stanford.edu" ]]; then
+  export ROS_MASTER_URI=http://localhost:11114
+else
+  export ROS_MASTER_URI=http://localhost:11311
+fi
 export ROS_IP=127.0.0.1
 export ROS_HOSTNAME=127.0.0.1
