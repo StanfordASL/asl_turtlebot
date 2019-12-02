fae2eb1c68c5e05c8cfbbe505062c1d92eac05e4
AA274 <richieling@hotmail.com>
2019-12-01 19:33:52 -0800

    updated rostb3 to not echo

--- rostb3.sh
+++ rostb3.sh
@@ -1,4 +1,4 @@
-echo "!!!!!!!!!!!!!!!!!!!!\nEdit this file (~/catkin_ws/src/asl_turtlebot/scripts/rostb3.sh) to replace <turtlebot_name> and <network_interface> below, then comment this line out by preceding it with a #\n!!!!!!!!!!!!!!!!!!!!"
+#echo "!!!!!!!!!!!!!!!!!!!!\nEdit this file (~/catkin_ws/src/asl_turtlebot/scripts/rostb3.sh) to replace <turtlebot_name> and <network_interface> below, then comment this line out by preceding it with a #\n!!!!!!!!!!!!!!!!!!!!"
 export ROS_MASTER_URI=http://didi.local:11311
 export ROS_IP=$(ifconfig ens33 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 export ROS_HOSTNAME=$(ifconfig ens33 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
