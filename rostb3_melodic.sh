4047f76643b727b958047885b3d4fae043b1c554
Jenna Lee <yunjung.jenna.lee@gmail.com>
2019-12-03 10:44:24 -0800

    remove laptop configuration files

--- rostb3_melodic.sh
+++
@@ -1,9 +0,0 @@
-#echo "!!!!!!!!!!!!!!!!!!!!\nEdit this file (~/catkin_ws/src/asl_turtlebot/scripts/rostb3.sh) to replace <turtlebot_name> and <network_interface> below, then comment this line out by preceding it with a #\n!!!!!!!!!!!!!!!!!!!!"
-export ROS_MASTER_URI=http://<turtlebot_name>.local:11311
-export ROS_IP=$(ifconfig wlp5s0 | grep 'inet' | head -1 | awk '{ print $2}')
-export ROS_HOSTNAME=$(ifconfig wlp5s0 | grep 'inet' | head -1 | awk '{ print $2}')
-
-echo "updated settings: "
-echo $ROS_MASTER_URI
-echo $ROS_IP
-echo $ROS_HOSTNAME
