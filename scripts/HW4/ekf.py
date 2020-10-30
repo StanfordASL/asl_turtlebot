b5d2bdd045fbbc94d82e24746545b8bf981103e0
Group08 <kmwang123@gmail.com>
2020-10-29 22:33:39 -0700

    added hw files

---
+++ scripts/HW4/ekf.py
@@ -0,0 +1,434 @@
+# Karen May Wang (kmwang14@stanford.edu)
+# AA274A
+# 10/29/2020
+import numpy as np
+import scipy.linalg    # you may find scipy.linalg.block_diag useful
+import turtlebot_model as tb
+import pdb
+class Ekf(object):
+    """
+    Base class for EKF Localization and SLAM.
+
+    Usage:
+        ekf = EKF(x0, Sigma0, R)
+        while True:
+            ekf.transition_update(u, dt)
+            ekf.measurement_update(z, Q)
+            localized_state = ekf.x
+    """
+
+    def __init__(self, x0, Sigma0, R):
+        """
+        EKF constructor.
+
+        Inputs:
+                x0: np.array[n,]  - initial belief mean.
+            Sigma0: np.array[n,n] - initial belief covariance.
+                 R: np.array[2,2] - control noise covariance (corresponding to dt = 1 second).
+        """
+        self.x = x0  # Gaussian belief mean
+        self.Sigma = Sigma0  # Gaussian belief covariance
+        self.R = R  # Control noise covariance (corresponding to dt = 1 second)
+
+    def transition_update(self, u, dt):
+        """
+        Performs the transition update step by updating (self.x, self.Sigma).
+
+        Inputs:
+             u: np.array[2,] - zero-order hold control input.
+            dt: float        - duration of discrete time step.
+        Output:
+            None - internal belief state (self.x, self.Sigma) should be updated.
+        """
+        g, Gx, Gu = self.transition_model(u, dt)
+        ########## Code starts here ##########
+        # TODO: Update self.x, self.Sigma.
+        self.x[0:3] = g[0:3]
+        self.Sigma = np.dot(np.dot(Gx,self.Sigma),Gx.T) + dt*np.dot(np.dot(Gu,self.R),Gu.T)
+
+        ########## Code ends here ##########
+
+    def transition_model(self, u, dt):
+        """
+        Propagates exact (nonlinear) state dynamics.
+
+        Inputs:
+             u: np.array[2,] - zero-order hold control input.
+            dt: float        - duration of discrete time step.
+        Outputs:
+             g: np.array[n,]  - result of belief mean propagated according to the
+                                system dynamics with control u for dt seconds.
+            Gx: np.array[n,n] - Jacobian of g with respect to belief mean self.x.
+            Gu: np.array[n,2] - Jacobian of g with respect to control u.
+        """
+        raise NotImplementedError("transition_model must be overriden by a subclass of EKF")
+
+    def measurement_update(self, z_raw, Q_raw):
+        """
+        Updates belief state according to the given measurement.
+
+        Inputs:
+            z_raw: np.array[2,I]   - matrix of I columns containing (alpha, r)
+                                     for each line extracted from the scanner
+                                     data in the scanner frame.
+            Q_raw: [np.array[2,2]] - list of I covariance matrices corresponding
+                                     to each (alpha, r) column of z_raw.
+        Output:
+            None - internal belief state (self.x, self.Sigma) should be updated.
+        """
+        z, Q, H = self.measurement_model(z_raw, Q_raw)
+        if z is None:
+            # Don't update if measurement is invalid
+            # (e.g., no line matches for line-based EKF localization)
+            return
+
+        ########## Code starts here ##########
+        # TODO: Update self.x, self.Sigma.
+        St = np.dot(H,np.dot(self.Sigma,H.T)) + Q
+        Kt = np.dot(self.Sigma,np.dot(H.T,np.linalg.inv(St)))
+        self.x = self.x + np.dot(Kt,z)
+        self.Sigma = self.Sigma - np.dot(Kt,np.dot(St,Kt.T))
+
+        ########## Code ends here ##########
+
+    def measurement_model(self, z_raw, Q_raw):
+        """
+        Converts raw measurements into the relevant Gaussian form (e.g., a
+        dimensionality reduction). Also returns the associated Jacobian for EKF
+        linearization.
+
+        Inputs:
+            z_raw: np.array[2,I]   - I lines extracted from scanner data in
+                                     columns representing (alpha, r) in the scanner frame.
+            Q_raw: [np.array[2,2]] - list of I covariance matrices corresponding
+                                     to each (alpha, r) column of z_raw.
+        Outputs:
+            z: np.array[2K,]   - measurement mean.
+            Q: np.array[2K,2K] - measurement covariance.
+            H: np.array[2K,n]  - Jacobian of z with respect to the belief mean self.x.
+        """
+        raise NotImplementedError("measurement_model must be overriden by a subclass of EKF")
+
+
+class EkfLocalization(Ekf):
+    """
+    EKF Localization.
+    """
+
+    def __init__(self, x0, Sigma0, R, map_lines, tf_base_to_camera, g):
+        """
+        EkfLocalization constructor.
+
+        Inputs:
+                       x0: np.array[3,]  - initial belief mean.
+                   Sigma0: np.array[3,3] - initial belief covariance.
+                        R: np.array[2,2] - control noise covariance (corresponding to dt = 1 second).
+                map_lines: np.array[2,J] - J map lines in columns representing (alpha, r).
+        tf_base_to_camera: np.array[3,]  - (x, y, theta) transform from the
+                                           robot base to camera frame.
+                        g: float         - validation gate.
+        """
+        self.map_lines = map_lines  # Matrix of J map lines with (alpha, r) as columns
+        self.tf_base_to_camera = tf_base_to_camera  # (x, y, theta) transform
+        self.g = g  # Validation gate
+        super(self.__class__, self).__init__(x0, Sigma0, R)
+
+    def transition_model(self, u, dt):
+        """
+        Turtlebot dynamics (unicycle model).
+        """
+
+        ########## Code starts here ##########
+        # TODO: Compute g, Gx, Gu using tb.compute_dynamics().
+        g, Gx, Gu = tb.compute_dynamics(self.x, u, dt, compute_jacobians=True)
+
+        ########## Code ends here ##########
+
+        return g, Gx, Gu
+
+    def measurement_model(self, z_raw, Q_raw):
+        """
+        Assemble one joint measurement and covariance from the individual values
+        corresponding to each matched line feature.
+        """
+        v_list, Q_list, H_list = self.compute_innovations(z_raw, Q_raw)
+        if not v_list:
+            print("Scanner sees {} lines but can't associate them with any map entries."
+                  .format(z_raw.shape[1]))
+            return None, None, None
+
+        ########## Code starts here ##########
+        # TODO: Compute z, Q.
+        # HINT: The scipy.linalg.block_diag() function may be useful.
+        # HINT: A list can be unpacked using the * (splat) operator. 
+        K = len(v_list)
+        z = np.array([item for items in v_list for item in items])
+        Q = Q_list[0]
+        H = H_list[0]
+        for i in range(1,K):
+            Q = scipy.linalg.block_diag(Q,Q_list[i])
+            H = np.vstack((H,H_list[i]))
+        ########## Code ends here ##########
+
+        return z, Q, H
+
+    def compute_innovations(self, z_raw, Q_raw):
+        """
+        Given lines extracted from the scanner data, tries to associate each one
+        to the closest map entry measured by Mahalanobis distance.
+
+        Inputs:
+            z_raw: np.array[2,I]   - I lines extracted from scanner data in
+                                     columns representing (alpha, r) in the scanner frame.
+            Q_raw: [np.array[2,2]] - list of I covariance matrices corresponding
+                                     to each (alpha, r) column of z_raw.
+        Outputs:
+            v_list: [np.array[2,]]  - list of at most I innovation vectors
+                                      (predicted map measurement - scanner measurement).
+            Q_list: [np.array[2,2]] - list of covariance matrices of the
+                                      innovation vectors (from scanner uncertainty).
+            H_list: [np.array[2,3]] - list of Jacobians of the innovation
+                                      vectors with respect to the belief mean self.x.
+        """
+        def angle_diff(a, b):
+            a = a % (2. * np.pi)
+            b = b % (2. * np.pi)
+            diff = a - b
+            if np.size(diff) == 1:
+                if np.abs(a - b) > np.pi:
+                    sign = 2. * (diff < 0.) - 1.
+                    diff += sign * 2. * np.pi
+            else:
+                idx = np.abs(diff) > np.pi
+                sign = 2. * (diff[idx] < 0.) - 1.
+                diff[idx] += sign * 2. * np.pi
+            return diff
+
+        hs, Hs = self.compute_predicted_measurements()
+
+        ########## Code starts here ##########
+        # TODO: Compute v_list, Q_list, H_list
+        # HINT: hs contains the J predicted lines, z_raw contains the I observed lines
+        # HINT: To calculate the innovation for alpha, use angle_diff() instead of plain subtraction
+        # HINT: Optionally, efficiently calculate all the innovations in a matrix V of shape [I, J, 2]. np.expand_dims() and np.dstack() may be useful.
+        # HINT: For each of the I observed lines, 
+        #       find the closest predicted line and the corresponding minimum Mahalanobis distance
+        #       if the minimum distance satisfies the gating criteria, add corresponding entries to v_list, Q_list, H_list
+        I = len(Q_raw) #number of lines extracted
+        J = len(hs[0,:]) #number of known map lines from camera frame
+        v_list = []
+        Q_list = []
+        H_list = []
+        for i in range(I):
+            #go through all known map lines in camera frames and compute Mahalanobis distance
+            dmin = None
+            vij_min = None
+            Q_min = None
+            H_min = None
+            for j in range(J):
+                vij = z_raw[:,i] - hs[:,j] #innovation 
+                Sij = np.dot(np.dot(Hs[j],self.Sigma),Hs[j].T) + Q_raw[i]
+                dij = np.dot(np.dot(vij.T,np.linalg.inv(Sij)),vij)
+                if dmin is None or dij < dmin:
+                    dmin = dij
+                    vij_min = vij
+                    Q_min = Q_raw[i]
+                    H_min = Hs[j]
+            if dmin < self.g**2:
+                v_list.append(vij_min)
+                Q_list.append(Q_min)
+                H_list.append(H_min)
+        ########## Code ends here ##########
+
+        return v_list, Q_list, H_list
+
+    def compute_predicted_measurements(self):
+        """
+        Given a single map line in the world frame, outputs the line parameters
+        in the scanner frame so it can be associated with the lines extracted
+        from the scanner measurements.
+
+        Input:
+            None
+        Outputs:
+                 hs: np.array[2,J]  - J line parameters in the scanner (camera) frame.
+            Hx_list: [np.array[2,3]] - list of Jacobians of h with respect to the belief mean self.x.
+        """
+        hs = np.zeros_like(self.map_lines)
+        Hx_list = []
+        for j in range(self.map_lines.shape[1]):
+            ########## Code starts here ##########
+            # TODO: Compute h, Hx using tb.transform_line_to_scanner_frame() for the j'th map line.
+            # HINT: This should be a single line of code.
+            h, Hx = tb.transform_line_to_scanner_frame(self.map_lines[:,j],self.x,self.tf_base_to_camera, compute_jacobian=True)
+
+            ########## Code ends here ##########
+
+            h, Hx = tb.normalize_line_parameters(h, Hx)
+            hs[:,j] = h
+            Hx_list.append(Hx)
+
+        return hs, Hx_list
+
+
+class EkfSlam(Ekf):
+    """
+    EKF SLAM.
+    """
+
+    def __init__(self, x0, Sigma0, R, tf_base_to_camera, g):
+        """
+        EKFSLAM constructor.
+
+        Inputs:
+                       x0: np.array[3+2J,]     - initial belief mean.
+                   Sigma0: np.array[3+2J,3+2J] - initial belief covariance.
+                        R: np.array[2,2]       - control noise covariance
+                                                 (corresponding to dt = 1 second).
+        tf_base_to_camera: np.array[3,]  - (x, y, theta) transform from the
+                                           robot base to camera frame.
+                        g: float         - validation gate.
+        """
+        self.tf_base_to_camera = tf_base_to_camera  # (x, y, theta) transform
+        self.g = g  # Validation gate
+        super(self.__class__, self).__init__(x0, Sigma0, R)
+
+    def transition_model(self, u, dt):
+        """
+        Combined Turtlebot + map dynamics.
+        Adapt this method from EkfLocalization.transition_model().
+        """
+        g = np.copy(self.x)
+        Gx = np.eye(self.x.size)
+        Gu = np.zeros((self.x.size, 2))
+
+        ########## Code starts here ##########
+        # TODO: Compute g, Gx, Gu.
+        # HINT: This should be very similar to EkfLocalization.transition_model() and take 1-5 lines of code.
+        # HINT: Call tb.compute_dynamics() with the correct elements of self.x
+        g, Gx[0:3,0:3], Gu[0:3,0:2] = tb.compute_dynamics(self.x[0:3], u, dt, compute_jacobians=True)
+        ########## Code ends here ##########
+
+        return g, Gx, Gu
+
+    def measurement_model(self, z_raw, Q_raw):
+        """
+        Combined Turtlebot + map measurement model.
+        Adapt this method from EkfLocalization.measurement_model().
+        
+        The ingredients for this model should look very similar to those for
+        EkfLocalization. In particular, essentially the only thing that needs to
+        change is the computation of Hx in self.compute_predicted_measurements()
+        and how that method is called in self.compute_innovations() (i.e.,
+        instead of getting world-frame line parameters from self.map_lines, you
+        must extract them from the state self.x).
+        """
+        v_list, Q_list, H_list = self.compute_innovations(z_raw, Q_raw)
+        if not v_list:
+            print("Scanner sees {} lines but can't associate them with any map entries."
+                  .format(z_raw.shape[1]))
+            return None, None, None
+
+        ########## Code starts here ##########
+        # TODO: Compute z, Q, H.
+        # Hint: Should be identical to EkfLocalization.measurement_model().
+        K = len(v_list)
+        z = np.array([item for items in v_list for item in items])
+        Q = Q_list[0]
+        H = H_list[0]
+        for i in range(1,K):
+            Q = scipy.linalg.block_diag(Q,Q_list[i])
+            H = np.vstack((H,H_list[i]))
+        ########## Code ends here ##########
+
+        return z, Q, H
+
+    def compute_innovations(self, z_raw, Q_raw):
+        """
+        Adapt this method from EkfLocalization.compute_innovations().
+        """
+        def angle_diff(a, b):
+            a = a % (2. * np.pi)
+            b = b % (2. * np.pi)
+            diff = a - b
+            if np.size(diff) == 1:
+                if np.abs(a - b) > np.pi:
+                    sign = 2. * (diff < 0.) - 1.
+                    diff += sign * 2. * np.pi
+            else:
+                idx = np.abs(diff) > np.pi
+                sign = 2. * (diff[idx] < 0.) - 1.
+                diff[idx] += sign * 2. * np.pi
+            return diff
+
+        hs, Hs = self.compute_predicted_measurements()
+
+        ########## Code starts here ##########
+        # TODO: Compute v_list, Q_list, H_list.
+        # HINT: Should be almost identical to EkfLocalization.compute_innovations(). What is J now?
+        # HINT: Instead of getting world-frame line parameters from self.map_lines, you must extract them from the state self.x.
+        I = len(Q_raw) #number of lines extracted
+        J = len(hs[0,:]) #number of known map lines from camera frame
+        v_list = []
+        Q_list = []
+        H_list = []
+        for i in range(I):
+            #go through all known map lines in camera frames and compute Mahalanobis distance
+            dmin = None
+            vij_min = None
+            Q_min = None
+            H_min = None
+            for j in range(J):
+                vij = z_raw[:,i] - hs[:,j] #innovation
+                Sij = np.dot(np.dot(Hs[j],self.Sigma),Hs[j].T) + Q_raw[i]
+                dij = np.dot(np.dot(vij.T,np.linalg.inv(Sij)),vij)
+                if dmin is None or dij < dmin:
+                    dmin = dij
+                    vij_min = vij
+                    Q_min = Q_raw[i]
+                    H_min = Hs[j]
+            if dmin < self.g**2:
+                v_list.append(vij_min)
+                Q_list.append(Q_min)
+                H_list.append(H_min)
+
+        ########## Code ends here ##########
+
+        return v_list, Q_list, H_list
+
+    def compute_predicted_measurements(self):
+        """
+        Adapt this method from EkfLocalization.compute_predicted_measurements().
+        """
+        J = (self.x.size - 3) // 2
+        hs = np.zeros((2, J))
+        Hx_list = []
+        for j in range(J):
+            idx_j = 3 + 2 * j
+            alpha, r = self.x[idx_j:idx_j+2]
+
+            Hx = np.zeros((2,self.x.size))
+
+            ########## Code starts here ##########
+            # TODO: Compute h, Hx.
+            # HINT: Call tb.transform_line_to_scanner_frame() for the j'th map line.
+            # HINT: The first 3 columns of Hx should be populated using the same approach as in EkfLocalization.compute_predicted_measurements().
+            # HINT: The first two map lines (j=0,1) are fixed so the Jacobian of h wrt the alpha and r for those lines is just 0. 
+            # HINT: For the other map lines (j>2), write out h in terms of alpha and r to get the Jacobian Hx.
+            h, Hx[:,0:3] = tb.transform_line_to_scanner_frame(np.array([alpha,r]),self.x[0:3],self.tf_base_to_camera, compute_jacobian=True)
+            # First two map lines are assumed fixed so we don't want to propagate
+            # any measurement correction to them.
+            xw = self.x[0]
+            yw = self.x[1]
+            thw = self.x[2]
+            xcam = self.tf_base_to_camera[0]
+            ycam = self.tf_base_to_camera[1]
+            if j >= 2:
+                Hx[:,idx_j:idx_j+2] = np.array([[1.0, 0],[np.sin(alpha)*( xw + xcam*np.cos(thw) - ycam*np.sin(thw) ) - np.cos(alpha)*(yw + xcam*np.sin(thw) + ycam*np.cos(thw)),1.0]])
+            ########## Code ends here ##########
+
+            h, Hx = tb.normalize_line_parameters(h, Hx)
+            hs[:,j] = h
+            Hx_list.append(Hx)
+
+        return hs, Hx_list
---
+++ scripts/HW4/turtlebot_model.py
@@ -0,0 +1,146 @@
+# Karen May Wang (kmwang14@stanford.edu)
+# AA274A
+# 10/29/2020
+import numpy as np
+import pdb
+EPSILON_OMEGA = 1e-3
+
+def compute_dynamics(xvec, u, dt, compute_jacobians=True):
+    """
+    Compute Turtlebot dynamics (unicycle model).
+
+    Inputs:
+                     xvec: np.array[3,] - Turtlebot state (x, y, theta).
+                        u: np.array[2,] - Turtlebot controls (V, omega).
+        compute_jacobians: bool         - compute Jacobians Gx, Gu if true.
+    Outputs:
+         g: np.array[3,]  - New state after applying u for dt seconds.
+        Gx: np.array[3,3] - Jacobian of g with respect to xvec.
+        Gu: np.array[3,2] - Jacobian of g with respect to u.
+    """
+    ########## Code starts here ##########
+    # TODO: Compute g, Gx, Gu
+    # HINT: To compute the new state g, you will need to integrate the dynamics of x, y, theta
+    # HINT: Since theta is changing with time, try integrating x, y wrt d(theta) instead of dt by introducing om
+    # HINT: When abs(om) < EPSILON_OMEGA, assume that the theta stays approximately constant ONLY for calculating the next x, y
+    #       New theta should not be equal to theta. Jacobian with respect to om is not 0.
+    x  = xvec[0]
+    y  = xvec[1]
+    th = xvec[2]
+    V  = u[0] 
+    om = u[1] 
+    #Compute new state g by integrating dynamics of x, y, theta
+    #We assume that V, om are constant over dt
+    g = np.zeros(3)
+    #if abs(om) < EPSILON_OMEGA, assume theta is constant for calculating x, y
+    #update om first
+    g[2] = th + om*dt
+    if abs(om) < EPSILON_OMEGA:
+       g[0] = x + V*np.cos(g[2])*dt 
+       g[1] = y + V*np.sin(g[2])*dt
+    else:
+       g[0] = x + (V/om)*(np.sin(g[2]) - np.sin(th)) 
+       g[1] = y - (V/om)*(np.cos(g[2]) - np.cos(th))
+    #Compute Jacobian with respect to state variables
+    # [dg[0]/dx, dg[0]/dy, dg[0]/dth]
+    # [dg[1]/dx, dg[1]/dy, dg[1]/dth]
+    # [dg[2]/dx, dg[2]/dy, dg[2]/dth]
+    if abs(om) < EPSILON_OMEGA:
+       Gx = np.array([[1.0,   0.0,  -V*np.sin(g[2])*dt],
+                      [0.0,    1.0,  V*np.cos(g[2])*dt],
+                      [0.0,    0.0,  1.0]])
+    else:
+       Gx = np.array([[1.0,   0.0,  (V/om)*(np.cos(g[2])-np.cos(th))],
+                      [0.0,    1.0, (V/om)*(np.sin(g[2])-np.sin(th))],
+                      [0.0,    0.0,  1.0]])
+    #Compute Jacobian with respect to control variables
+    # [dg[0]/dV, dg[0]/dom]
+    # [dg[1]/dV, dg[1]/dom]
+    # [dg[2]/dV, dg[2]/dom]
+    if abs(om) < EPSILON_OMEGA:
+       Gu = np.array([[ np.cos(g[2])*dt, -V*np.sin(g[2])*dt**2],
+                      [np.sin(g[2])*dt,  V*np.cos(g[2])*dt**2],
+                      [0,         dt]])
+
+    else:
+       Gu = np.array([[ (1./om)*(np.sin(g[2])-np.sin(th)), -(V/om**2)*(np.sin(g[2])-np.sin(th)) + (V/om)*np.cos(g[2])*dt],
+                      [-(1./om)*(np.cos(g[2])-np.cos(th)),  (V/om**2)*(np.cos(g[2])-np.cos(th)) + (V/om)*np.sin(g[2])*dt],
+                      [0,         dt]])
+    ########## Code ends here ##########
+
+    if not compute_jacobians:
+        return g
+
+    return g, Gx, Gu
+
+def transform_line_to_scanner_frame(line, x, tf_base_to_camera, compute_jacobian=True):
+    """
+    Given a single map line in the world frame, outputs the line parameters
+    in the scanner frame so it can be associated with the lines extracted
+    from the scanner measurements.
+
+    Input:
+                     line: np.array[2,] - map line (alpha, r) in world frame.
+                        x: np.array[3,] - pose of base (x, y, theta) in world frame.
+        tf_base_to_camera: np.array[3,] - pose of camera (x, y, theta) in base frame.
+         compute_jacobian: bool         - compute Jacobian Hx if true.
+    Outputs:
+         h: np.array[2,]  - line parameters in the scanner (camera) frame.
+        Hx: np.array[2,3] - Jacobian of h with respect to x.
+    """
+    alpha, r = line
+
+    ########## Code starts here ##########
+    # TODO: Compute h, Hx
+    # HINT: Calculate the pose of the camera in the world frame (x_cam, y_cam, th_cam), a rotation matrix may be useful.
+    # HINT: To compute line parameters in the camera frame h = (alpha_in_cam, r_in_cam), 
+    #       draw a diagram with a line parameterized by (alpha,r) in the world frame and 
+    #       a camera frame with origin at x_cam, y_cam rotated by th_cam wrt to the world frame
+    # HINT: What is the projection of the camera location (x_cam, y_cam) on the line r? 
+    # HINT: To find Hx, write h in terms of the pose of the base in world frame (x_base, y_base, th_base)
+    #line in world frame
+    xw = x[0]
+    yw = x[1]
+    thw = x[2]
+    xcam = tf_base_to_camera[0]
+    ycam = tf_base_to_camera[1]
+    thcam = tf_base_to_camera[2]
+    h = np.zeros(2)
+    h[0] = alpha - (thw + thcam)
+    h[1] = r- (np.cos(alpha)*(xw + xcam*np.cos(thw)-ycam*np.sin(thw)) + np.sin(alpha)*(yw+xcam*np.sin(thw)+ycam*np.cos(thw)) )
+    #Jacobian is
+    #dh[0]/dx dh[0]/dy dh[0]dth
+    #dh[1]/dx dh[1]/dy dh[1]dth
+    Hx = np.array([[0.0,            0.0,            -1.0],
+                   [-np.cos(alpha), -np.sin(alpha), -np.cos(alpha)*(-xcam*np.sin(thw)-ycam*np.cos(thw)) - np.sin(alpha)*(xcam*np.cos(thw)-ycam*np.sin(thw)) ]])
+    ########## Code ends here ##########
+
+    if not compute_jacobian:
+        return h
+
+    return h, Hx
+
+
+def normalize_line_parameters(h, Hx=None):
+    """
+    Ensures that r is positive and alpha is in the range [-pi, pi].
+
+    Inputs:
+         h: np.array[2,]  - line parameters (alpha, r).
+        Hx: np.array[2,n] - Jacobian of line parameters with respect to x.
+    Outputs:
+         h: np.array[2,]  - normalized parameters.
+        Hx: np.array[2,n] - Jacobian of normalized line parameters. Edited in place.
+    """
+    alpha, r = h
+    if r < 0:
+        alpha += np.pi
+        r *= -1
+        if Hx is not None:
+            Hx[1,:] *= -1
+    alpha = (alpha + np.pi) % (2*np.pi) - np.pi
+    h = np.array([alpha, r])
+
+    if Hx is not None:
+        return h, Hx
+    return h
