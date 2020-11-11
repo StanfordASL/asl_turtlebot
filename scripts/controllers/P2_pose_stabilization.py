6e3ec01f6b9baafb099226a2fb66397c4a5a3959
Group08 <kmwang123@gmail.com>
2020-11-10 23:23:38 -0800

    added new world

--- scripts/controllers/P2_pose_stabilization.py
+++ scripts/controllers/P2_pose_stabilization.py
@@ -1,8 +1,7 @@
-# Karen May Wang (kmwang14@stanford.edu)
-# AA274A
-# 10/29/2020
 import numpy as np
 from utils import wrapToPi
+import rospy
+from std_msgs.msg import Float64
 
 # command zero velocities once we are this close to the goal
 RHO_THRES = 0.05
@@ -19,6 +18,13 @@ class PoseController:
         self.V_max = V_max
         self.om_max = om_max
 
+        #*** ADDED G.S. 10/28/20 **************************************************
+        self.alpha_pub = rospy.Publisher('/controller/alpha', Float64, queue_size=1)
+        self.delta_pub = rospy.Publisher('/controller/delta', Float64, queue_size=1)
+        self.rho_pub   = rospy.Publisher('/controller/rho', Float64, queue_size=1)
+        #***************************************************************************
+
+
     def load_goal(self, x_g, y_g, th_g):
         """ Loads in a new goal position """
         self.x_g = x_g
@@ -30,41 +36,73 @@ class PoseController:
         Inputs:
             x,y,th: Current state
             t: Current time (you shouldn't need to use this)
-        Outputs:
+        Outputs: 
             V, om: Control actions
 
         Hints: You'll need to use the wrapToPi function. The np.sinc function
         may also be useful, look up its documentation
         """
         ########## Code starts here ##########
-        #change of coordinates so that origin is at (x_g,y_g,th_g)
-        xstar = x-self.x_g
-        ystar = y-self.y_g
-        thstar = th-self.th_g
-        #rotate coordinate so that xprime axis is along the unicycle main axis
-        xprime =  np.cos(self.th_g)*xstar + np.sin(self.th_g)*ystar
-        yprime = -np.sin(self.th_g)*xstar + np.cos(self.th_g)*ystar
-        #rho is distance of the reference point of the unicycle w.r.t goal location
-        rho = np.sqrt(xprime**2 + yprime**2)
-        #alpha is angle of pointing vector to the goal w.r.t. the unicycle main axis
-        alpha = np.arctan2(yprime,xprime) - thstar + np.pi
-        #delta is angle of the same pointing vector w.r.t. the XN axis
-        delta = alpha + thstar
-        #make sure alpha and delta remain in the range [-pi,pi]
-        alpha = wrapToPi(alpha)
-        delta = wrapToPi(delta)
+        
+        
+        xg = self.x_g
+        yg = self.y_g
+        thg = self.th_g
 
-        #command zero velocities once any of these thresholds are met
-        if rho <= RHO_THRES and np.abs(alpha) <= ALPHA_THRES and np.abs(delta) <= DELTA_THRES:
-            V  = 0.0
-            om = 0.0
-        else:
-            V = self.k1*rho*np.cos(alpha)
-            om = self.k2*alpha + self.k1*np.sinc(alpha/np.pi)*np.cos(alpha)*(alpha + self.k3*delta)
+        # compute range to goal
+
+        rho = np.sqrt(  ( x - xg )**2.0 + ( y - yg )**2.0 ) 
+
+        # compute absolute bearing to target
+
+        beta = np.arctan2( ( yg - y ), ( xg - x ) )
+
+        # compute relative bearing to target
+
+        #alpha = utils.wrapToPi( beta - theta )
+        alpha = wrapToPi( beta - th )
+
+        # compute the polar-coordinate angle delta (in desired position frame)
+
+        #delta = utils.wrapToPi( beta - th_g )
+        delta = wrapToPi( beta - thg )
+
+        # compute the controls
+
+        V = self.k1 * rho * np.cos( alpha )
+                
+        tmp = np.sinc( alpha / np.pi ) * np.cos( alpha )
+        om = self.k2 * alpha + self.k1 * tmp * ( alpha + self.k3 * delta )
+
+        #print(' V = ', V )
+        #print(' om = ', om )
+        """
+        #Publishing our computed alpha, delta, and rho values for navigation
+        self.alpha_pub = rospy.Publisher('controller/alpha', Float64, queue_size=1)
+        self.delta_pub = rospy.Publisher('controller/delta', Float64, queue_size=1)
+        self.rho_pub   = rospy.Publisher('controller/rho', Float64, queue_size=1)
+        rospy.init_node('pose_controller', anonymous=True)
+
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            alpha_ros = Float64()
+            self.alpha_pub.publish(alpha_ros)
+            delta_ros = Float64()
+            self.delta_pub.publish(delta_ros)
+            rho_ros = Float64()
+            self.rho_pub.publish(rho_ros)
+            rate.sleep() 
+        """             
+        self.alpha_pub.publish(Float64(alpha))
+	self.delta_pub.publish(Float64(delta))
+	self.rho_pub.publish(Float64(rho))
+
+        #print('PUBLISHED alpha delta rho')
         ########## Code ends here ##########
 
         # apply control limits
         V = np.clip(V, -self.V_max, self.V_max)
+                                
         om = np.clip(om, -self.om_max, self.om_max)
 
         return V, om
