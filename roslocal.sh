bb7d2d7e2ca79421e99869fac2c411f010787c9b
rdyro <robert.dyro@gmail.com>
2021-10-12 10:46:24 -0700

    Updated rostb3 and roslocal to work under both 20.04 and 16.04 and be a little more verbose.

--- roslocal.sh
+++ roslocal.sh
@@ -1,3 +1,12 @@
-export ROS_MASTER_URI=http://localhost:11311
-export ROS_IP=127.0.0.1
-export ROS_HOSTNAME=127.0.0.1
+# rest of the script needs no changes ##########################################
+export ROS_MASTER_URI="http://localhost:11311"
+export ROS_IP="127.0.0.1"
+export ROS_HOSTNAME="127.0.0.1"
+
+echo "Success!"
+echo "Set the following values:"
+echo "    ROS_MASTER_URI=$ROS_MASTER_URI"
+echo "    ROS_HOSTNAME=$ROS_HOSTNAME"
+echo "    ROS_IP=$ROS_IP"
+echo "Optionally check that ROS_HOSTNAME and ROS_IP match this machine's ip"
+################################################################################
--- rostb3.sh
+++ rostb3.sh
@@ -1,4 +1,38 @@
-echo "!!!!!!!!!!!!!!!!!!!!\nEdit this file (~/catkin_ws/src/asl_turtlebot/scripts/rostb3.sh) to replace <turtlebot_name> and <network_interface> below, then comment this line out by preceding it with a #\n!!!!!!!!!!!!!!!!!!!!"
-export ROS_MASTER_URI=http://<turtlebot_name>.local:11311
-export ROS_IP=$(ifconfig <network_interface> | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-export ROS_HOSTNAME=$(ifconfig <network_interface> | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+# user variables section #######################################################
+# TURTLEBOT_NAME=<insert your turtlebot name here, e.g. "asteroid">
+# e.g. TURTLEBOT_NAME=arthur
+TURTLEBOT_NAME="" # TODO insert your value in quotes
+################################################################################
+
+# user variables validation section ############################################
+if [ -z "$TURTLEBOT_NAME" ]; then
+  echo "######################## Error! ########################"
+  echo ""
+  echo "Edit this file (~/catkin_ws/src/asl_turtlebot/scripts/rostb3.sh)" \
+    "to populate the TURTLEBOT_NAME variable"
+  echo ""
+  echo "######################## Error! ########################"
+  return 1
+fi
+################################################################################
+
+# rest of the script needs not changes #########################################
+
+# extracts the first (usually there's only one) linux wireless interface's name
+INTERFACE_NAME=$(ifconfig | grep -Eo '^wl[0-9a-Z]+' | head -n1)
+
+# sets the ROS_MASTER_IP to be the robot
+export ROS_MASTER_URI="http://$TURTLEBOT_NAME.local:11311"
+
+# sets the own hostname and ip to match the local wireless LAN ip
+export ROS_IP=$(ifconfig $INTERFACE_NAME | grep -Eo '192\.168\.[0-9]+\.[0-9]+' \
+  | head -n1)
+export ROS_HOSTNAME=$ROS_IP
+
+echo "Success!"
+echo "Set the following values:"
+echo "    ROS_MASTER_URI=$ROS_MASTER_URI"
+echo "    ROS_HOSTNAME=$ROS_HOSTNAME"
+echo "    ROS_IP=$ROS_IP"
+echo "Optionally check that ROS_HOSTNAME and ROS_IP match this machine's ip"
+################################################################################
