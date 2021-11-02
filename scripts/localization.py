620d1e9e7ded3b7fe569d7eefef20397a9140966
Robin Brown <robinbrown@Robins-MacBook-Pro.local>
2021-11-02 11:13:29 -0700

    param names for HW4

--- scripts/localization.py
+++ scripts/localization.py
@@ -15,8 +15,8 @@ from HW4.maze_sim_parameters import LineExtractionParams, NoiseParams, MapParams
 class LocalizationParams:
 
     def __init__(self, verbose=False):
-        self.mc = rospy.get_param("~mc", False)
-        self.num_particles = rospy.get_param("~num_particles", 100)
+        self.mc = rospy.get_param("/rosparam/mc", False)
+        self.num_particles = rospy.get_param("/rosparam/num_particles", 100)
         if verbose:
             print(self)
 
